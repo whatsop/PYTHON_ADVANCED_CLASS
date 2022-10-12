808c413df7f68401b25c25fba9c679708321de7d
whatsop <kasimov.arup@gmail.com>
2022-10-12 15:48:35 -0400

    ADD custom houdini launch batch script

---
+++ 01_tools/custom_houdini_launch_batch.bat
@@ -0,0 +1,27 @@
+@echo off
+
+:: find all the information about launching houdini using command line here https://www.sidefx.com/docs/houdini/ref/commandline.html
+
+echo --------------------------------------------------------------------------------------------
+echo launching houdini 19.5.303
+echo --------------------------------------------------------------------------------------------
+echo SCRIPTS :
+echo saving_reminter_tool will be launched
+echo --------------------------------------------------------------------------------------------
+
+
+:: houdini path
+set houdini="C:\Program Files\Side Effects Software\Houdini 19.5.303\bin\houdinifx.exe"
+
+:: scrips variables
+set saving_reminder_tool=D:\PYTHON_ADVANCED_COURSE\tools\saving_reminder_tool\main.py
+set desktop_name=__AK__
+
+
+:: launch houdini
+%houdini% waitforui %saving_reminder_tool% -desktop %desktop_name%
+
+
+
+ 
+cmd /k
